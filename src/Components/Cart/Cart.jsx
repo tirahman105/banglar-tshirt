ca19041a07d62e66c45edeffcc82f96fb24ddfc1
tirahman105 <tirahman105@gmail.com>
2023-04-08 09:44:39 +0600

    remove from cart and toast added

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Cart = ({cart, handleRemoveFromCart}) => {
+    return (
+        <div>
+            <h2>Order Summary: {cart.length}</h2>
+            {
+                cart.map(tshirt=> <p 
+                    key={tshirt._id}
+                    >{tshirt.name} <button onClick={()=>handleRemoveFromCart(tshirt._id)}>x</button></p>)
+            }
+        </div>
+    );
+};
+
+export default Cart;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css';
+import toast from 'react-hot-toast';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart]= useState([]);
+
+    const handleAddToCart = tshirt => {
+        const exist = cart.find(ts=> ts._id === tshirt._id);
+        if(exist){
+            toast('you have already added this product')
+        }else{
+
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+    }
+    const handleRemoveFromCart = id =>{
+        const remaining = cart.filter(ts=> ts._id !== id);
+        setCart(remaining);
+    }
     return (
-        <div>
-            <h1>This is home: {tshirts.length}</h1>
+        <div className='home-container'>
+            <div className="t-shirt-container">
+                
+            {
+                tshirts.map(tshirt => <Tshirt
+                key={tshirt._id}
+                tshirt={tshirt}
+                handleAddToCart = {handleAddToCart}
+                ></Tshirt>)
+            }
+            </div>
+
+            <div className="cart-container">
+                <Cart cart = {cart}
+                handleRemoveFromCart={handleRemoveFromCart}></Cart>
+            </div>
+
         </div>
     );
 };
